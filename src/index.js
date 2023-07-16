b4ef89d262106bca52cef2d7ce8e2027955fbeff
much98 <much981120@gmail.com>
2023-07-16 10:58:56 +0800

    .

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ const { Client, IntentsBitField } = require('discord.js')
 
 const client = new Client({
     intents: [
-        Intents.ALL
-    ]
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
 })
 
-require('dotenv').config();
-const botToken = process.env.BOT_TOKEN;
+const config = require('./config.json'); /*change to safe_config for your use*/
+
+const botToken = config.botToken;
 
 client.login(botToken)
