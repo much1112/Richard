50b29f14b91e24ed471dcff3f04e753dbebefbfa
much98 <much981120@gmail.com>
2023-07-16 10:07:55 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const { Client, IntentsBitField } = require('discord.js')
 
 const client = new Client({
     intents: [
-        IntentsBitField.Flags.Guilds, 
+        Intents.ALL
     ]
 })
+
+client.login("MTEyOTk1MTQ5MDY4MTg4MDY2Ng.GPYAmh.qreSua8RLh-No20909-gteFk840KbsY3twPVns")
